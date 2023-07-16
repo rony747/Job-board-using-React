c137798b23055b92db60d6c52832ca64098f5570
Touhidul Islam Rony <touhid_rony@yahoo.com>
2023-07-16 10:08:44 +0600

    added some icons

--- src/components/Job.jsx
+++ src/components/Job.jsx
@@ -1,7 +1,7 @@
 import {Badge} from "@/components/ui/badge"
 import {Button} from "@/components/ui/button.jsx";
 import {Link} from "react-router-dom";
-import {Building2, ChevronLeft, MapPin} from "lucide-react";
+import {AlarmClock, Building2, ChevronLeft, DollarSign, MapPin} from "lucide-react";
 
 
 function Job({job}) {
@@ -15,22 +15,36 @@ function Job({job}) {
                         <h2 className={'font-bold text-xl'}>{job.job_title}</h2>
                         <div className={'my-2 border-t-[1px] border-gray-100 border-b-[1px]'}>
                             <div className={'text-base text-gray-500 flex justify-between'}><span
-                                className={'font-bold flex items-center'}><Building2 className="h-4 w-4 text-blue-700 mr-1" />: {job.company_name}</span> , <span className={'flex items-center'}> <MapPin className="h-4 w-4 text-blue-700 mr-3" /> {job.location}</span></div>
+                                className={'font-bold flex items-center'}><Building2
+                                className="h-4 w-4 text-blue-700 mr-1"/>: {job.company_name}</span> <span
+                                className={'flex items-center'}> <MapPin
+                                className="h-4 w-4 text-blue-700 mr-3"/> {job.location}</span></div>
                         </div>
                         <p>{job.job_description.substring(0, 100) + '...'}</p>
                         <div className={'flex gap-2 mt-3 mb-3'}>
-                            <Badge className={'bg-blue-100 text-blue-700 rounded-sm hover:bg-blue-300'}>{job.employment_type}</Badge>
+                            <Badge
+                                className={'bg-blue-100 text-blue-700 rounded-sm hover:bg-blue-300'}>{job.employment_type}</Badge>
                             <Badge
                                 className={'bg-blue-100 text-blue-700 rounded-sm hover:bg-blue-300'}>Salary: {Math.round(job.salary / 1000) + 'k'}</Badge>
-                            <Badge className={'bg-blue-100 text-blue-700 rounded-sm hover:bg-blue-300'}>{job.job_level}</Badge>
+                            <Badge
+                                className={'bg-blue-100 text-blue-700 rounded-sm hover:bg-blue-300'}>{job.job_level}</Badge>
                         </div>
+
+                        <div className={'mt-3  bg-gray-100 px-2 py-1'}>
+                            <div className={'text-sm text-gray-500 flex justify-between'}><span
+                                className={'font-bold flex items-center'}><DollarSign
+                                className="h-4 w-4 text-blue-700 mr-1"/>: {Math.round(job.salary / 1000) + 'k'}</span> <span
+                                className={'flex items-center'}> <AlarmClock
+                                className="h-4 w-4 text-blue-700 mr-3"/> {job.application_deadline}</span></div>
+                        </div>
+
                     </div>
 
-                    <div className="job_btn flex gap-3">
-                        <Button asChild className={'text-gray-700 mt-3 w-1/2  bg-gray-200 hover:text-white'}>
+                    <div className="job_btn flex gap-3 mt-2">
+                        <Button asChild className={'text-gray-700  w-1/2  bg-gray-200 hover:text-white'}>
                             <Link to={`/find-job/all/${job.id}`}>View Details</Link>
                         </Button>
-                        <Button asChild className={'text-blue-100 mt-3 w-1/2  bg-blue-700 hover:text-white'}>
+                        <Button asChild className={'text-blue-100 w-1/2  bg-blue-700 hover:text-white'}>
                             <Link to={`/find-job/all/${job.id}/apply`}>Apply Now</Link>
                         </Button>
                     </div>
--- src/components/JobLists.jsx
+++ src/components/JobLists.jsx
@@ -8,6 +8,9 @@ function JobLists() {
     let allJobs = filteredJobs.length > 0 ? filteredJobs :jobs
     return (
         <>
+            <div className="job_sortBar flex justify-between mb-3">
+                <div className="total_num"><h3 className={'font-bold text-xl text-gray-600'}>Showing {allJobs.length} Jobs</h3></div>
+            </div>
             <div className={'grid gap-4 grid-cols-3'}>
                 {allJobs?.map(job => {
                     return <Job key={job.contact_email} job={job}/>
